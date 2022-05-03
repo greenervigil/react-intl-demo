12e3822339a449eec88712fa9f0476c62860d103
Vigil, Daniel <Daniel.Vigil@cengage.com>
2022-05-03 11:26:16 -0400

    adding languages

---
+++ src/i18n/locales.js
@@ -0,0 +1,7 @@
+export const LOCALES = {
+    USENGLISH: 'en-US',
+    BRITISH: 'en_GB',
+    SWEDISH: 'sv',
+    SPANISH: 'es',
+    ARABIC: 'ar',
+};
---
+++ src/i18n/messages.js
@@ -0,0 +1,29 @@
+import { LOCALES } from "./locales";
+
+export const messages = {
+    [LOCALES.USENGLISH]: {
+        "app.welcome": "Welcome",
+        "app.color-label": "What is your favorite color?",
+        "app.date-label": "Pick a date",
+    },
+    [LOCALES.BRITISH]: {
+        "app.welcome": "Welcome",
+        "app.color-label": "What is your favourite colour?",
+        "app.date-label": "Pick a date",
+    },
+    [LOCALES.SPANISH]: {
+        "app.welcome": "Bienvenido",
+        "app.color-label": "¿Cuál es tu color favorito?",
+        "app.date-label": "Elija una fecha",
+    }, 
+    [LOCALES.SWEDISH]: {
+        "app.welcome": "Välkommen",
+        "app.color-label": "",
+        "app.date-label": "Välj ett datum",
+    }, 
+    [LOCALES.ARABIC]: {
+        "app.welcome": "أهلا بك",
+        "app.color-label": "ما هو لونك المفضل؟",
+        "app.date-label": "اختر تاريخًا",
+    }
+}
